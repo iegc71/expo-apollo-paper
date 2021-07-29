c0c4d20106d594687dda9689ced5e1a30bea7bd0
Ivan Garcia <iegc71@gmail.com>
2021-07-29 08:42:03 -0500

    apollo client 3

--- App.tsx
+++ App.tsx
@@ -3,15 +3,30 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import theme from 'utils/theme';
+import { client } from 'utils/graphql/client';
+import { ApolloProvider, gql } from '@apollo/client';
 
 export default function App(): JSX.Element {
+  client
+    .query({
+      query: gql`
+        query GetRates {
+          rates(currency: "USD") {
+            currency
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
   return (
-    <PaperProvider theme={theme}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!!!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </PaperProvider>
+    <ApolloProvider client={client}>
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!!!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </PaperProvider>
+    </ApolloProvider>
   );
 }
 const styles = StyleSheet.create({
